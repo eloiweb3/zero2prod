371c9e4163cd258e6530263ee9a4b01bbe0c784f
Eloi <recycled_loops@hotmail.com>
2025-07-13 12:24:00 +0200

    health test

--- src/startup.rs
+++ src/startup.rs
@@ -1,18 +1,20 @@
 use std::net::TcpListener;
 use actix_web::{web, App, HttpServer};
 use actix_web::dev::Server;
+use sqlx::PgConnection;
 
 use crate::routes::health_check::health_check;
 use crate::routes::subscriptions::subscribe;
 
-pub  fn run(listener: TcpListener ) ->Result<Server, std::io::Error>{
+pub  fn run(listener: TcpListener, connection: PgConnection ) ->Result<Server, std::io::Error>{
     let server = HttpServer::new(|| {
-        App::new()
+        App::new()  
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(connection)
     })
         .listen(listener)?
         .run();
 
     Ok(server)
-}
+}       
